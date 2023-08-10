f1246cab94388c1f0acc29f369358ceb4845e948
Mirza <mirza@ethans-mbp.lan>
2023-08-10 13:43:38 -0500

    Unit 7 completed

--- 02-IDE_exercises/07b-project/reverse_list.py
+++ 02-IDE_exercises/07b-project/reverse_list.py
@@ -1,6 +1,15 @@
 def reverse_list (a_list: list[int]) -> None:
-    a_list = [x[-1] for x in a_list]
-
-vals = [2, -5, 10, 9]
+    length: int = len(a_list) - 1
+    counter = -1
+    middle_list = []
+    while length > -1:
+            middle_list.append(a_list[counter])
+            length -= 1
+            counter -=1
+    counter2 = 0
+    while counter2 < len(a_list):
+          a_list[counter2] = middle_list [counter2]
+          counter2 += 1
+vals = [2, -5, 10, 9,7,6,5,4]
 reverse_list(vals)
-print(vals)  # This should print [9, 10, -5, 2]
+print(vals)
