ec16639081712d20b39cf374369918526f6b6c98
Mirza <mirza@ethans-mbp.lan>
2023-08-11 12:15:09 -0500

    Adding part 2 of Unit 7

--- 02-IDE_exercises/07b-project/reverse_list.py
+++ 02-IDE_exercises/07b-project/reverse_list.py
@@ -6,10 +6,4 @@ def reverse_list (a_list: list[int]) -> None:
             middle_list.append(a_list[counter])
             length -= 1
             counter -=1
-    counter2 = 0
-    while counter2 < len(a_list):
-          a_list[counter2] = middle_list [counter2]
-          counter2 += 1
-vals = [2, -5, 10, 9,7,6,5,4]
-reverse_list(vals)
-print(vals)
+    a_list[:] = middle_list
---
+++ 02-IDE_exercises/08a-project/count_letters.py
@@ -0,0 +1,8 @@
+def count_letters(a_string: str) -> dict:
+    new_dict = {}
+    for i in a_string:
+        if i.upper() == i.lower() and not new_dict[i.upper()]:
+            new_dict[i.upper()] = 1
+        elif i.upper() == i.lower():
+            new_dict[i.upper()] +=1
+    return new_dict
