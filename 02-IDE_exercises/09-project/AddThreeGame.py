dbde5a23ba1db3716ed43e2d1fce6b760fd8d41e
Mirza <mirza@ethans-mbp.lan>
2023-08-15 11:17:42 -0500

    Fixed project 9, all tests OK

--- 02-IDE_exercises/09-project/AddThreeGame.py
+++ 02-IDE_exercises/09-project/AddThreeGame.py
@@ -10,9 +10,6 @@ class AddThreeGame:
         return self._current_state
 
     def make_move (self, which_player: str, number: int):
-        if self._player1_choices + self._player2_choices == [1,2,3,4,5,6,7,8,9]:
-            self._current_state = 'DRAW'
-            return True
         if which_player != 'first' and which_player != 'second':
             return False
         if self._whose_turn == 'first' and which_player == 'second':
@@ -22,7 +19,9 @@ class AddThreeGame:
         if self._whose_turn == 'first' and which_player == 'first':
             if number not in range(1,10):
                 return False
-            if number not in self._player1_choices and number not in self._player2_choices:
+            if number  in self._player1_choices or number in self._player2_choices:
+                return False
+            else:
                 self._player1_choices.append(number)
                 if len(self._player1_choices) < 3:
                     self._whose_turn = 'second'
@@ -30,30 +29,37 @@ class AddThreeGame:
                 else:
                     if len(self._player1_choices) == 3:
                         if self._player1_choices[1] + self._player1_choices[2] + self._player1_choices[0] == 15:
-                            self._current_state = 'FIRST WON'
+                            self._current_state = 'FIRST_WON'
                             return True
                         else:
                             self._whose_turn = 'second'
                             return True
                     if len(self._player1_choices) == 4:
                         if self._player1_choices[1] + self._player1_choices[2] + self._player1_choices[3] == 15 or self._player1_choices[1] + self._player1_choices[2] + self._player1_choices[0] ==15 or self._player1_choices[1] + self._player1_choices[3] + self._player1_choices[0] == 15 or self._player1_choices[0] + self._player1_choices[2]+self._player1_choices[3] == 15:
-                            self._current_state = 'FIRST WON'
+                            self._current_state = 'FIRST_WON'
+                            return True
+                        elif len(self._player1_choices) + len(self._player2_choices) == 9:
+                            self._current_state = 'DRAW'
                             return True
                         else:
                             self._whose_turn = 'second'
                             return True
                     if len(self._player1_choices) == 5:
                         if self._player1_choices[1] + self._player1_choices[2] + self._player1_choices[3] == 15 or self._player1_choices[1] + self._player1_choices[2] + self._player1_choices[4] == 15 or self._player1_choices[1] + self._player1_choices[2] + self._player1_choices[0] == 15 or self._player1_choices[1] + self._player1_choices[3] + self._player1_choices[4] == 15 or self._player1_choices[1] + self._player1_choices[3] + self._player1_choices[0] == 15 or self._player1_choices[1] + self._player1_choices[4] + self._player1_choices[0] == 15 or self._player1_choices[2] + self._player1_choices[3] + self._player1_choices[4] == 15 or self._player1_choices[2] + self._player1_choices[3] + self._player1_choices[0] == 15 or self._player1_choices[2] + self._player1_choices[4] + self._player1_choices[0] == 15 or self._player1_choices[3] + self._player1_choices[4] + self._player1_choices[0] == 15:
-                            self._current_state = 'FIRST WON'
+                            self._current_state = 'FIRST_WON'
+                            return True
+                        elif len(self._player1_choices) + len(self._player2_choices) == 9:
+                            self._current_state = 'DRAW'
                             return True
                         else:
                             self._whose_turn = 'second'
                             return True
-                return False
         if self._whose_turn == 'second' and which_player == 'second':
             if number not in range(1,10):
                 return False
-            if number not in self._player1_choices and number not in self._player2_choices:
+            if number in self._player1_choices or number in self._player2_choices:
+                return False
+            else:
                 self._player2_choices.append(number)
                 if len(self._player2_choices) < 3:
                     self._whose_turn = 'first'
@@ -61,32 +67,31 @@ class AddThreeGame:
                 else:
                     if len(self._player2_choices) == 3:
                         if self._player2_choices[1] + self._player2_choices[2] + self._player2_choices[0] == 15:
-                            self._current_state = 'SECOND WON'
+                            self._current_state = 'SECOND_WON'
                             return True
                         else:
                             self._whose_turn = 'first'
                             return True
                     if len(self._player2_choices) == 4:
                         if self._player2_choices[1] + self._player2_choices[2] + self._player2_choices[3] == 15 or self._player2_choices[1] + self._player2_choices[2] + self._player2_choices[0] ==15 or self._player2_choices[1] + self._player2_choices[3] + self._player2_choices[0] == 15 or self._player2_choices[0] + self._player2_choices[2] + self._player2_choices[3] == 15:
-                            self._current_state = 'SECOND WON'
+                            self._current_state = 'SECOND_WON'
+                            return True
+                        elif len(self._player1_choices) + len(self._player2_choices) == 9:
+                            self._current_state = 'DRAW'
                             return True
                         else:
                             self._whose_turn = 'first'
                             return True
                     if len(self._player2_choices) == 5:
                         if self._player2_choices[1] + self._player2_choices[2] + self._player2_choices[3] == 15 or self._player2_choices[1] + self._player2_choices[2] + self._player2_choices[4] == 15 or self._player2_choices[1] + self._player2_choices[2] + self._player2_choices[0] == 15 or self._player2_choices[1] + self._player2_choices[3] + self._player2_choices[4] == 15 or self._player2_choices[1] + self._player2_choices[3] + self._player2_choices[0] == 15 or self._player2_choices[1] + self._player2_choices[4] + self._player2_choices[0] == 15 or self._player2_choices[2] + self._player2_choices[3] + self._player2_choices[4] == 15 or self._player2_choices[2] + self._player2_choices[3] + self._player2_choices[0] == 15 or self._player2_choices[2] + self._player2_choices[4] + self._player2_choices[0] == 15 or self._player2_choices[3] + self._player2_choices[4] + self._player2_choices[0] == 15:
-                            self._current_state = 'SECOND WON'
+                            self._current_state = 'SECOND_WON'
+                            return True
+                        elif len(self._player1_choices) + len(self._player2_choices) == 9:
+                            self._current_state = 'DRAW'
                             return True
                         else:
                             self._whose_turn = 'first'
                             return True
-                return False
 
-game = AddThreeGame()
-game.make_move('first', 3)
-game.make_move('second', 7)
-game.make_move('first', 8)
-game.make_move('second', 2)
-game.make_move('first', 5)
-game.make_move('second', 6)
-print(game.get_current_state())
+
+
